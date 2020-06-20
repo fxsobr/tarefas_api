015967ca7a2dbb8ee91d08cf38012e01779faacc
Marcelo Ceolin <fxsobr@gmail.com>
2020-06-20 11:39:19 -0300

    adicionado variavel de ambiente no docker-compose e corrigido forma de busca da variavel no _init.py

--- sistema/__init__.py
+++ sistema/__init__.py
@@ -1,8 +1,11 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 
 app = Flask(__name__)
-app.config.from_object('sistema.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
 
 api = Api(app)
 
