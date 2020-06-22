d5927153930417c8b14373c9a3237238c4f7c3ef
Marcelo Ceolin <mceolin@unidavi.edu.br>
2020-06-22 15:37:12 -0300

    inicializado migrate e sqlalchemy

--- sistema/__init__.py
+++ sistema/__init__.py
@@ -1,11 +1,15 @@
 import os
 
 from flask import Flask
+from flask_migrate import Migrate
 from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settings)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 api = Api(app)
 
