9ef0c22da6b2816c1ce6cefabcd5cadee922c15f
Marcelo Ceolin <mceolin@unidavi.edu.br>
2020-06-23 15:37:17 -0300

    ajuste funcionarios

--- sistema/api/models/funcionario.py
+++ sistema/api/models/funcionario.py
@@ -12,7 +12,7 @@ class FuncionarioModel(db.Model):
     rg = db.Column(db.String(20), nullable=False)
     data_nascimento = db.Column(db.Date, nullable=False)
     email = db.Column(db.String(50), nullable=False)
-    telefone_celular = db.Column(db.Integer, nullable=False)
+    telefone_celular = db.Column(db.String(12), nullable=False)
     endereco_rua = db.Column(db.String(512), nullable=False)
     endereco_bairro = db.Column(db.String(50), nullable=False)
     endereco_numero = db.Column(db.Integer, nullable=False)
--- sistema/api/schemas/funcionario.py
+++ sistema/api/schemas/funcionario.py
@@ -5,4 +5,5 @@ from sistema.api.models.funcionario import FuncionarioModel
 class FuncionarioSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = FuncionarioModel
+        include_fk = True
         load_instance = True
