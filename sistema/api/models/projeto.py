a684c0173d078afcc3994aa11d95609e880d2a20
Marcelo Ceolin <mceolin@unidavi.edu.br>
2020-06-23 11:19:34 -0300

    criado model projeto

---
+++ sistema/api/models/projeto.py
@@ -0,0 +1,27 @@
+from typing import List
+
+from sistema import db
+
+
+class ProjetoModel(db.Model):
+    __tablename__ = "projeto"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    nome = db.Column(db.String(50), nullable=False)
+    descricao = db.Column(db.String(100), nullable=False)
+
+    @classmethod
+    def find_all(cls) -> List["ProjetoModel"]:
+        return cls.query.all()
+
+    @classmethod
+    def find_by_id(cls, _id: int) -> "ProjetoModel":
+        return cls.query.filter_by(id=_id).first()
+
+    def save_to_db(self) -> None:
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self) -> None:
+        db.session.delete(self)
+        db.session.commit()
